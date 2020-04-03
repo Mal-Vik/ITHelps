c00b562045b40192a0b8518bab92aeeb261d565b
Viktor <malyshkin.viktor@icloud.com>
2020-04-03 10:14:43 +0300

    Correct vue.config.js & babel.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,25 @@
-import VueFriendlyIframe from "./src/plugins/vue-friendly-iframe";
+// eslint-disable-next-line no-unused-vars
+// import VueFriendlyIframe from './src/plugins/vue-friendly-iframe'
 
 module.exports = {
     assetsDir: 'static',
     configureWebpack: {
         resolve: {
             extensions: ['*', '.js', '.vue', '.json'],
-            alias: {
-                '@': '/Users/Viktor/WebstormProjects/MyProjectsVUE/src',
-                src: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src',
-                app: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app',
-                assets: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/assets',
-                '@components': '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/components',
-                layouts: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/layouts',
-                locale: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/locale',
-                mixins: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/mixins',
-                pages: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/pages',
-                services: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/services',
-                store: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/store',
-                utils: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/utils',
-            },
+            // alias: {
+            //     '@': '/Users/Viktor/WebstormProjects/MyProjectsVUE/src',
+            //     src: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src',
+            //     app: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app',
+            //     assets: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/assets',
+            //     '@components': '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/components',
+            //     layouts: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/layouts',
+            //     locale: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/locale',
+            //     mixins: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/mixins',
+            //     pages: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/pages',
+            //     services: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/services',
+            //     store: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/store',
+            //     utils: '/Users/Viktor/WebstormProjects/MyProjectsVUE/src/app/utils',
+            // },
         },
     },
     transpileDependencies: ['vuetify', 'vue-friendly-iframe', 'VueFriendlyIframe'],
