d1f5d12b73d77e3def1ca7b32e51d08a6267ff68
Viktor <malyshkin.viktor@icloud.com>
2020-04-04 12:23:20 +0300

    Refactoring header

---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <AppBar
+            :isActiveDrawerLeft="isActiveDrawerLeft"
+            :isActiveDrawerRight="isActiveDrawerRight"
+            @emitChangeDisplayDrawerLeft="changeDisplayDrawerLeft"
+            @emitChangeDisplayDrawerRight="changeDisplayDrawerRight"
+        />
+        <NavigationDrawerLeft :isActiveDrawerLeft="isActiveDrawerLeft" />
+        <NavigationDrawerRight :isActiveDrawerRight="isActiveDrawerRight" @emitDisplaySubDrawerRight="showNavigationSubDrawerRight" />
+        <NavigationSubDrawerRight :isActiveSubDrawerRight="isActiveSubDrawerRight" :dataSubDrawerRight="dataSubDrawerRight" />
+    </div>
+</template>
+
+<script>
+    import AppBar from '../components/header/AppBar'
+    import NavigationDrawerLeft from '../components/header/navigation_boxes/NavigationDrawerLeft'
+    import NavigationDrawerRight from '../components/header/navigation_boxes/NavigationDrawerRight'
+    import NavigationSubDrawerRight from '../components/header/navigation_boxes/NavigationSubDrawerRight'
+
+    export default {
+        name: 'Header',
+        components: {
+            AppBar,
+            NavigationDrawerLeft,
+            NavigationDrawerRight,
+            NavigationSubDrawerRight,
+        },
+        data: () => ({
+            isActiveDrawerLeft: true,
+            isActiveDrawerRight: true,
+            isActiveSubDrawerLeft: false,
+            isActiveSubDrawerRight: false,
+            dataSubDrawerRight: '',
+        }),
+        methods: {
+            changeDisplayDrawerLeft(isActiveDrawerLeft) {
+                this.isActiveDrawerLeft = isActiveDrawerLeft
+            },
+            changeDisplayDrawerRight(isActiveDrawerRight) {
+                this.isActiveDrawerRight = isActiveDrawerRight
+            },
+            showSubNavigationDrawerLeft() {
+                this.isActiveSubDrawerLeft = !this.isActiveSubDrawerLeft
+            },
+            showNavigationSubDrawerRight(item) {
+                let isActiveSubDrawerRight = this.isActiveSubDrawerRight
+                this.isActiveSubDrawerRight = !isActiveSubDrawerRight
+                this.dataSubDrawerRight = item
+            },
+        },
+    }
+</script>
